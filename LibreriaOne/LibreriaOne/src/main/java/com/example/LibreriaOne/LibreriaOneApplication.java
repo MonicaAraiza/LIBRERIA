fee4d4192f76ce38e7cbaba1e557e18a3a888020
MonicaAraiza <156965012+MonicaAraiza@users.noreply.github.com>
2024-07-06 16:30:57 -0600

    Add files via upload

---
+++ LibreriaOne/LibreriaOne/src/main/java/com/example/LibreriaOne/LibreriaOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibreriaOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaOneApplication.class, args);
+	}
+
+}
---
+++ LibreriaOne/LibreriaOne/src/test/java/com/example/LibreriaOne/LibreriaOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibreriaOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
